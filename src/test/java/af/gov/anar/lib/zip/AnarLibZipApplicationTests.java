f8293f2cfa52df9326e51f94a3fbf5e5fdf768e4
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-13 12:30:44 +0430

    anar zip library

---
+++ src/test/java/af/gov/anar/lib/zip/AnarLibZipApplicationTests.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.zip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnarLibZipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
